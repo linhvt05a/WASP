2f836e7cf6de8d4f650cce3b11775676742db600
hoang linh <hoanglinh6794@gmail.com>
2021-04-08 20:48:27 +0700

    init redux saga

---
+++ WASPApp/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import {Provider} from 'react-redux'
+import {store} from './src/redux/stores'
+
+const WASApp = () => {
+    return(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    )
+}
+AppRegistry.registerComponent(appName, () => WASApp);
---
+++ WASPApp/src/redux/stores/index.tsx
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import rootReducers from '../reducers'
+import rootSagas from '../sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+    rootReducers,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(rootSagas)
+
+export {
+    store
+}
